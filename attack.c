f5a39d01bd160f0476da93404a35f5d0235b574c
cstakechi <z535044z@mails.cc.ehime-u.ac.jp>
2016-07-14 20:34:50 +0900

    puroguramu

---
+++ attack.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "make_spn.h"
+
+#define T 8000
+void binary(int num,int array_binary[4]){ //二進数へ変換
+	int i;
+	for(i=0;i<4;i++){
+			array_binary[3-i] = num % 2;
+			num = num / 2;
+		}
+		if(i!= 4){
+			for(;i<4;i++){
+				array_binary[3-i] = 0;
+			}
+		}
+}
+int decimal(int array_binary[4]){  //10進数へ変換
+	int i,temp=0;
+	for(i=0;i<4;i++)
+		temp += array_binary[i] * pow(2,3-i);
+	return temp;
+}
+
+int pi_s_im(int num){ //p_s^{-1}
+	switch (num){
+	case 0:
+		return 0xe;
+	case 1:
+		return 0x3;
+	case 2:
+		return 0x4;
+	case 3:
+		return 0x8;
+	case 4:
+		return 0x1;
+	case 5:
+		return 0xc;
+	case 6:
+		return 0xa;
+	case 7:
+		return 0xf;
+	case 8:
+		return 0x7;
+	case 9:
+		return 0xd;
+	case 0xa:
+		return 0x9;
+	case 0xb:
+		return 0x6;
+	case 0xc:
+		return 0xb;
+	case 0xd:
+		return 0x2;
+	case 0xe:
+		return 0x0;
+	case 0xf:
+		return 0x5;
+	default:
+		return 100;
+	}
+}
+
+int main(){
+
+	int i,j,k;
+	int count[16][16];
+	int plaintext[4],ciphertext[4];
+	int plaintext_binary[4][4],ciphertext_binary[4][4];
+	int v2[4],v4[4];
+	int u2,u4;
+	int u2_binary[4],u4_binary[4];
+	int temp[4];
+	int z;
+	int t;
+	int maxkey1,maxkey2;
+
+	for(i=0;i<16;i++)
+		for(j=0;j<16;j++)
+			count[i][j] = 0;
+
+	for(t=0;t<T;t++){
+		for(i=0;i<4;i++)
+			plaintext[i] = rand()%16;
+
+		spn(plaintext[0],plaintext[1],plaintext[2],plaintext[3],ciphertext);
+		for(i=0;i<4;i++){
+				binary(plaintext[i],plaintext_binary[i]);
+		}
+		for(i=0;i<4;i++)
+			binary(ciphertext[i],ciphertext_binary[i]);
+
+		for(i=0;i<16;i++){
+			for(j=0;j<16;j++){
+			//v_2の作成
+				binary(i,temp);
+				for(k=0;k<4;k++)
+					v2[k] = temp[k] ^ ciphertext_binary[1][k];
+			//v_4の作成
+				binary(j,temp);
+				for(k=0;k<4;k++)
+					v4[k] = temp[k] ^ ciphertext_binary[3][k];
+			//u2の作成
+				u2 = decimal(v2);
+				u2 = pi_s_im(u2);
+			//u4の作成
+				u4 = decimal(v4);
+				u4 = pi_s_im(u4);
+				binary(u2,u2_binary);
+				binary(u4,u4_binary);
+			//zの計算
+				z = plaintext_binary[1][0] ^ plaintext_binary[1][2] ^ plaintext_binary[1][3] ^ u2_binary[1] ^ u2_binary[3] ^ u4_binary[1] ^ u4_binary[3];
+				if(z == 0)
+					count[i][j]++;
+			}
+		}
+	}
+
+	int max = -1;
+	for(i=0;i<16;i++){
+		for(j=0;j<16;j++){
+			count[i][j] = count[i][j] - (t / 2);
+			if(count[i][j] < 0)
+				count[i][j] = -count[i][j];
+			if(count[i][j] > max){
+				max = count[i][j];
+				maxkey1 = i;
+				maxkey2 = j;
+			}
+		}
+	}
+	printf("%x %x\n",maxkey1,maxkey2);
+	return 0;
+}
---
+++ make_spn.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<math.h>
+#include"make_spn.h"
+
+#define lm 16
+
+void make_binary(int num,int array_binary[4]){
+	int i;
+		for(i=0;i<4;i++){
+				array_binary[3-i] = num % 2;
+				num = num / 2;
+			}
+		if(i!= 4){
+			for(;i<4;i++){
+				array_binary[3-i] = 0;
+			}
+		}
+}
+
+int pi_s[lm] = {0xE, 0x4, 0xD, 0x1,
+               0x2, 0xF, 0xB, 0x8,
+               0x3, 0xA, 0x6, 0xC,
+               0x5, 0x9, 0x0, 0x7};
+
+int pi_p[lm] = {  1,   5,   7,  13,
+                 2,   6,  10,  14,
+                 3,   7,  11,  15,
+                 4,   8,  12,  16};
+
+
+void pip(int w1[4],int w2[4], int w3[4], int w4[4],int p,int temp[4]){
+	p -= 1;
+	w1[p] = temp[0];
+	w2[p] = temp[1];
+	w3[p] = temp[2];
+	w4[p] = temp[3];
+}
+
+
+
+
+void spn(int a, int b, int c, int d, int cipher[4]){
+
+	int k[32]={0,0,1,1,1,0,1,0,1,0,0,1,0,1,0,0,1,1,0,1,0,1,1,0,0,0,1,1,1,1,1,1};
+	int w1[4],w2[4],w3[4],w4[4];
+	int u1[4],u2[4],u3[4],u4[4];
+	int v1,v2,v3,v4;
+	int temp[4];
+
+	make_binary(a,w1);
+	make_binary(b,w2);
+	make_binary(c,w3);
+	make_binary(d,w4);
+
+	int i,round;
+	for(round=0;round<3;round++){
+		for(i=0;i<4;i++){
+			u1[i] = w1[i] ^ k[i+(round*4)];
+			u2[i] = w2[i] ^ k[i+4+(round*4)];
+			u3[i] = w3[i] ^ k[i+8+(round*4)];
+			u4[i] = w4[i] ^ k[i+12+(round*4)];
+		}
+		v1 = decimal(u1);
+		v2 = decimal(u2);
+		v3 = decimal(u3);
+		v4 = decimal(u4);
+		v1 = pi_s[v1];
+		v2 = pi_s[v2];
+		v3 = pi_s[v3];
+		v4 = pi_s[v4];
+		make_binary(v1,temp);
+		pip(w1,w2,w3,w4,1,temp);
+		make_binary(v2,temp);
+		pip(w1,w2,w3,w4,2,temp);
+		make_binary(v3,temp);
+		pip(w1,w2,w3,w4,3,temp);
+		make_binary(v4,temp);
+		pip(w1,w2,w3,w4,4,temp);
+	}
+
+	for(i=0;i<4;i++){
+		u1[i] = w1[i] ^ k[i+(round*4)];
+		u2[i] = w2[i] ^ k[i+4+(round*4)];
+		u3[i] = w3[i] ^ k[i+8+(round*4)];
+		u4[i] = w4[i] ^ k[i+12+(round*4)];
+	}
+	round++;
+	v1 = decimal(u1);
+	v2 = decimal(u2);
+	v3 = decimal(u3);
+	v4 = decimal(u4);
+	v1 = pi_s[v1];
+	v2 = pi_s[v2];
+	v3 = pi_s[v3];
+	v4 = pi_s[v4];
+	make_binary(v1,w1);
+	make_binary(v2,w2);
+	make_binary(v3,w3);
+	make_binary(v4,w4);
+
+	for(i=0;i<4;i++){
+			u1[i] = w1[i] ^ k[i+(round*4)];
+			u2[i] = w2[i] ^ k[i+4+(round*4)];
+			u3[i] = w3[i] ^ k[i+8+(round*4)];
+			u4[i] = w4[i] ^ k[i+12+(round*4)];
+	}
+	a = decimal(u1);
+	b = decimal(u2);
+	c = decimal(u3);
+	d = decimal(u4);
+	cipher[0] = a;
+	cipher[1] = b;
+	cipher[2] = c;
+	cipher[3] = d;
+
+}
