db3448f0c1f70e4b173f4ca21726e6d9797e7d64
cstakechi <z535044z@mails.cc.ehime-u.ac.jp>
2016-07-17 22:25:46 +0900

    algorithm3.5

---
+++ subbytes.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<math.h>
+
+	int a[8] = {1,1,0,0,1,0,1,0};
+	int c[8] = {1,1,0,0,0,1,1,0};
+
+
+void fieldinv(int a[7]){
+
+	int temp,i;
+
+	for(i=0;i<4;i++){
+		temp = a[i];
+		a[i] = a[7-i];
+		a[7-i] = temp;
+	}
+}
+
+
+int binarytofield(int a[7]){
+	int z=0,i;
+	for(i=0;i<8;i++)
+		z += a[i] * pow(2,i);
+	return z;
+}
+
+void fieldtobinary(int z,int a[7]){
+	int i;
+		for(i=0;i<8;i++){
+				a[8-i] = z % 2;
+				z = z / 2;
+			}
+}
+
+
+
+int main(){
+
+	int b[8];
+	int z;
+	int i,j;
+
+	z = binarytofield(a);
+	printf("%x\n",z);
+	if(z != 0)
+		fieldinv(a);
+
+	for(i=0;i<8;i++){
+		b[i] = a[i] + c[i];
+
+		j = (i+4) % 8;
+		b[i] +=  a[j];
+
+		j = (i+5) % 8;
+		b[i] += a[j];
+
+		j = (i+6) % 8;
+		b[i] += a[j];
+
+		j = (i+7) % 8;
+		b[i] += a[j];
+
+		b[i] %= 2;
+
+	}
+
+	for(i=7;i>-1;i--)
+		printf("%d",b[i]);
+
+	return 0;
+}
